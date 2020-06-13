11ec4e99bee8353a5c89d622e9d6aab860f95d0a
Sal Hamidi <hamidisa87@gmail.com>
2020-06-13 04:35:04 -0400

    Test

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,49 @@
-const fibonacci = function() {
+let arrayLength = 4
+fibonaci(arrayLength)
 
+function fibonaci(arrayLength)
+{
+
+    arrayLength = Number(arrayLength)
+    console.log(arrayLength)
+    if (arrayLength < 0) 
+    {
+        console.log("OOPS")
+    }
+    else    
+        {    
+    
+    let array = [1]
+    let counter = arrayLength
+    console.log(array.length)
+    console.log(counter)
+        while (array.length != arrayLength)
+        {
+            currentValue = array[arrayLength-counter]
+
+            if (arrayLength-counter===0)
+            {
+                previousValue = 0
+            }
+            else 
+            {
+                previousValue = array[arrayLength-counter-1]
+            }
+
+
+            console.log(currentValue)
+            counter = counter - 1
+            newValue = currentValue + previousValue
+            array.push(newValue)
+            console.log(array)
+
+        }
+    console.log(array[array.length]+array[array.length-1])
+
+
+
+
+    }
 }
+console.log('fibonacci')
 
-module.exports = fibonacci
